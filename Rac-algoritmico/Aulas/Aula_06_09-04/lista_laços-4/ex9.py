754e37ca4dcca52857ea3e1e7d5e26ec74723aa0
Apolo mor <apolomsilva@gmail.com>
2026-04-14 18:08:40 -0300

    ex 9 e 10

--- Rac-algoritmico/Aulas/Aula_06_09-04/lista_laços-4/ex9.py
+++ Rac-algoritmico/Aulas/Aula_06_09-04/lista_laços-4/ex9.py
@@ -1,3 +1,27 @@
-'''Elaborar um programa em Python que converta um número decimal em hexadecimal,
-fazendo uso do método de divisões sucessivas.'''
-binario = str(input("Digite um numero em binário: ")) 
+'''Elaborar um programa em Python que converta um número decimal em hexadecimal, fazendo uso do método de divisões sucessivas.
+Para converter um número decimal para hexadecimal, divida sucessivamente o número decimal por 16,
+anotando o resto de cada divisão, até que o quociente seja zero. Os restos, lidos da última divisão para a primeira 
+(ordem inversa), formam o número hexadecimal, substituindo valores de 10 a 15 por A a F. '''
+decimal = int(input("Digite um numero em decimal: "))
+if decimal == 0:
+    hexadecimal = "0"
+else:
+    hexadecimal = ""
+    while decimal > 0:
+        resto_divisão = decimal % 16
+        if resto_divisão == 10:
+            hexadecimal = 'A' + hexadecimal
+        elif resto_divisão == 11:
+            hexadecimal = 'B' + hexadecimal
+        elif resto_divisão == 12:
+            hexadecimal = 'C' + hexadecimal
+        elif resto_divisão == 13:
+            hexadecimal = 'D' + hexadecimal
+        elif resto_divisão == 14:
+            hexadecimal = 'E' + hexadecimal
+        elif resto_divisão == 15:
+            hexadecimal = 'F' + hexadecimal
+        else:
+            hexadecimal = str(resto_divisão) + hexadecimal
+        decimal = decimal // 16
+print("O número em hexadecimal é:", hexadecimal)
