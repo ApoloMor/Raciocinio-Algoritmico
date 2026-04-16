d208c759a8c1c4dda551e6400c356fc1358a4f9a
Apolo <apolomsilva@gmail.com>
2026-04-16 15:29:57 +0000

    parcial pprof5

--- prova/pprof5.py
+++ prova/pprof5.py
@@ -1,2 +1,13 @@
 #entrada »» aabcc   saida »» a2b1c2
 txt = input("Digite a string: ")
+r = ""
+ch = ""
+cont =0
+for c in txt:
+    if c!= ch:
+        if cont > 0:
+            r += f"{c}{cont}"
+        ch = c
+        cont += 1
+    else:
+        cont += 1
