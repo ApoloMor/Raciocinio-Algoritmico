12b44ddedaad4b442545aebcecd0394af32e02a5
Apolo <apolomsilva@gmail.com>
2026-04-16 15:44:44 +0000

    aaaaa

--- prova/pprof5.py
+++ prova/pprof5.py
@@ -1,13 +1,14 @@
-#entrada »» aabcc   saida »» a2b1c2
 txt = input("Digite a string: ")
 r = ""
 ch = ""
-cont =0
+cont = 0
 for c in txt:
     if c!= ch:
         if cont > 0:
-            r += f"{c}{cont}"
+            r += f"{ch}{cont}"
         ch = c
-        cont += 1
+        cont = 1
     else:
         cont += 1
+r+= ch+ str(cont)
+print(r)
--- prova/prova.py
+++ prova/prova.py
@@ -1,3 +1,22 @@
+"""
+txt = str(input("Digite uma palavra ou frase: "))
+cont = 1 
+final = "" 
+anterior = "" 
+primeira = True 
+for l in txt: 
+  if primeira: 
+    anterior = l 
+    primeira = False 
+  else: 
+    if anterior == l: 
+      cont += 1 
+      final += l + str(cont) 
+      else: 
+        final += l + "1" 
+        cont = 0 
+print(final)
+"""
 txt = str(input("Digite uma palavra ou frase: "))
 cont = 1
 final = ""
