390c17692d75a5af5fe522941bce37b365039fc5
Apolo <apolomsilva@gmail.com>
2026-04-22 09:11:22 -0300

    exemplo 5 e 6

--- Rac-algoritmico/Aulas/Aula7_22-04/exemplo.py
+++ Rac-algoritmico/Aulas/Aula7_22-04/exemplo.py
@@ -1,3 +1,11 @@
 """
 Funções de objeto
+fazer uma função que não modifica um objeto
+ex: elevar um numero  ao quadrado
 """
+def quadrado(num) #toda função/variavel dentro de uma def é local, ou seja esta só pode ser usada dentro de def
+    quad = num * num
+    return quad 
+n = 3
+q = quadrado(n)
+print("O quadrado de {n} é {q}")
