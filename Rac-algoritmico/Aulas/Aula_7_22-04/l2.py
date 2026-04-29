08a668fad6ce9e4a56428c13107cbb1cdb23ac26
ApoloMor <apolomsilva@gmail.com>
2026-04-29 10:14:43 -0300

    exercicio 2 slide listas 05

--- Rac-algoritmico/Aulas/Aula_7_22-04/l2.py
+++ Rac-algoritmico/Aulas/Aula_7_22-04/l2.py
@@ -1,17 +1,16 @@
 '''Criar um programa que solicite ao usuário 2 listas com 5
 elementos cada. Como resultado, criar uma terceira lista que
 intercala os elementos das duas listas.'''
-l3 = [ ]
-cont = 0
-while cont < 6:
-    i1 = input("digite um elemento:")
+l3 = []
+cont = 1
+while cont < 7:
+    i1 = input(f"digite o {cont} elemento:")
     l1 = [i1]
-    i2 = input("digite um elemento:")
+    i2 = input(f"digite o {cont} elemento:")
     l2 = [i2]
+    for i in l1:
+        l3.append(i)
+        for j in l2:
+            l3.append(j)
     cont += 1
-
-for i in l1:
-    l3.append(i)
-    for j in l2:
-        l3.append(j)
-print(l1, l2, l3)
+print(l3)
