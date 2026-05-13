bd21851f0157fe102f4b92ef4c0a9183ed842766
Apolo mor <apolomsilva@gmail.com>
2026-05-12 21:42:11 -0300

    l16

---
+++ Rac-algoritmico/Aulas/Aula_7_lista_24_ex/l16.py
@@ -0,0 +1,38 @@
+'''Utilize uma lista para resolver o problema a seguir. Uma empresa paga seus vendedores com base em comissões.
+O vendedor recebe $200 por semana mais 9 por cento de suas vendas brutas daquela semana. Por exemplo, um
+vendedor que teve vendas brutas de $3000 em uma semana recebe $200 mais 9 por cento de $3000, ou seja, um
+total de $470. Escreva um programa (usando um array de contadores) que determine quantos vendedores
+receberam salários nos seguintes intervalos de valores:
+a. $200 - $299
+b. $300 - $399
+c. $400 - $499
+d. $500 - $599
+e. $600 - $699
+f. $700 - $799
+g. $800 - $899
+h. $900 - $999
+i. $1000 em diante
+Desafio: Crie ma fórmula para chegar na posição da lista a partir do salário, sem fazer vários ifs aninhados.'''
+faixas = [0] * 9
+while True:
+    vendas = float(input("Digite o valor das vendas brutas (-1 para sair): "))
+    if vendas == -1:
+        break
+    salario = 200 + (vendas * 0.09)
+    indice = int(salario // 100) - 2
+    if indice >= 8:
+        indice = 8
+    faixas[indice] += 1
+intervalos = [
+    "$200 - $299",
+    "$300 - $399",
+    "$400 - $499",
+    "$500 - $599",
+    "$600 - $699",
+    "$700 - $799",
+    "$800 - $899",
+    "$900 - $999",
+    "$1000 em diante"]
+print("Quantidade de vendedores por faixa salarial:")
+for i in range(len(faixas)):
+    print(f"{intervalos[i]}: {faixas[i]}")
