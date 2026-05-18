8edf20b8accbf82a01cbb71fef271bd8c2437059
Apolo mor <apolomsilva@gmail.com>
2026-05-18 18:48:22 -0300

    l2 dentro dos conteudos passados

--- Rac-algoritmico/TDEs_rac_algoritmos/lista-7-funcoes/l2.py
+++ Rac-algoritmico/TDEs_rac_algoritmos/lista-7-funcoes/l2.py
@@ -1,7 +1,11 @@
 def imprimir_padrao(n):
     for i in range(1, n + 1):
+        linha = ""
+
         for j in range(1, i + 1):
-            print(j, end=" ")
-        print()
+            linha += str(j) + " "
+
+        print(linha)
+
 num = int(input("Digite um valor para n: "))
 imprimir_padrao(num)
